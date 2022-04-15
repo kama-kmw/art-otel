5730af5a0b626e2d8064c409d8bbba0b24d90668
Surha <surha_dag@mail.ru>
2022-04-15 15:14:26 +0300

    добавил header.php

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!-- Header Section Begin -->
+<header class="header-section">
+    <div class="container-fluid">
+        <div class="inner-header">
+            <!-- <div class="logo">
+                    <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                </div> -->
+            <div class="logo"><a href="#">Art Hotel</a></div>
+
+            <div class="nav-right">
+                <a href="#" class="primary-btn">Забронировать</a>
+            </div>
+            <nav class="main-menu mobile-menu">
+                <ul>
+                    <li class="active"><a href="./index.html">Главная</a></li>
+                    <li><a href="rooms.php">Номера</a></li>
+                    <li><a href="galery.php">Галерея</a></li>
+                    <li><a href="contact.php">Контакты</a></li>
+                    <li>f</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<!-- Header End -->
